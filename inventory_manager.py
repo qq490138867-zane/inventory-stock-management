c414dc6cfec254a3ba31b166c4a6b40265bf5cf0
Qian Cao <qq490138867@gmail.com>
2026-07-01 12:16:46 +0100

    Update inventory_manager.py

--- inventory_manager.py
+++ inventory_manager.py
@@ -1,1 +1,9 @@
+class InventoryManager:
+    def __init__(self):
+        self.products = []
 
+    def add_product(self, product):
+        self.products.append(product)
+
+    def get_all_products(self):
+        return self.products
