5c546abe01580fae3b55b9817f777cb6148fe0a7
Qian Cao <qq490138867@gmail.com>
2026-07-01 12:22:31 +0100

    Update file_handler.py

--- file_handler.py
+++ file_handler.py
@@ -1,1 +1,23 @@
+import json
+import os
 
+
+DATA_FILE = "data/inventory.json"
+
+
+def load_data():
+    """Load product data from JSON file."""
+    if not os.path.exists(DATA_FILE):
+        return []
+
+    with open(DATA_FILE, "r") as file:
+        try:
+            return json.load(file)
+        except json.JSONDecodeError:
+            return []
+
+
+def save_data(products):
+    """Save product data to JSON file."""
+    with open(DATA_FILE, "w") as file:
+        json.dump(products, file, indent=4)
