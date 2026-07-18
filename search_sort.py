1319255230f5e3a1a85224cf7d1ee30dd6376b9c
Qian Cao <qq490138867@gmail.com>
2026-07-18 13:37:59 +0100

    Update search_sort.py

--- search_sort.py
+++ search_sort.py
@@ -1,1 +1,32 @@
+def search_by_name(products, keyword):
+    """
+    Search products by name.
+    """
+    keyword = keyword.lower()
 
+    return [
+        product
+        for product in products
+        if keyword in product["name"].lower()
+    ]
+
+
+def sort_by_name(products):
+    """
+    Sort products by name.
+    """
+    return sorted(products, key=lambda product: product["name"].lower())
+
+
+def sort_by_price(products):
+    """
+    Sort products by price.
+    """
+    return sorted(products, key=lambda product: product["price"])
+
+
+def sort_by_quantity(products):
+    """
+    Sort products by quantity.
+    """
+    return sorted(products, key=lambda product: product["quantity"])
